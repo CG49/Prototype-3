49613ed68d30dd975f1f8a129b62e9db56f25e33
Chaitanya Ghule <38359593+CG49@users.noreply.github.com>
2026-07-08 02:18:16 +0530

    Updated Animation and fixed obstacle issue

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -44,10 +44,10 @@ public class PlayerController : MonoBehaviour
     {
         if (!isOnGround || isGameOver) return;
 
+        playerAnim.SetTrigger("Jump_trig");
+
         playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         isOnGround = false;
-
-        playerAnim.SetTrigger("Jump_trig");
     }
 
     void OnCollisionEnter(Collision collision)
@@ -60,10 +60,10 @@ public class PlayerController : MonoBehaviour
             isOnGround = true;
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            OnGameOver?.Invoke();
+
             isGameOver = true;
             Debug.Log("Game Over!!!");
-
-            OnGameOver?.Invoke();
 
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
